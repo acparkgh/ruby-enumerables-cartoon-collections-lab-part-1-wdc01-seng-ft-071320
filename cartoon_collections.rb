489b6f7d4f2876bc70c329d9fc4081e8f0ef2b43
Andrew Park <acpark123@gmail.com>
2020-06-10 05:56:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   
-  array.each do |character|
-    puts "Hello #{character}!"
-  end
+  array.each { |character| puts "Hello #{character}!" }
   
 end
 
